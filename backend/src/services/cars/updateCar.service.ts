993882108fbfac446fc0ea2309f168e89f503581
Renan.dev <dev.rfcsb@gmail.com>
2023-08-07 14:36:36 -0400

    feat: Delete and Update

---
+++ backend/src/services/cars/updateCar.service.ts
@@ -0,0 +1,28 @@
+import { Repository } from "typeorm";
+import { TResponseCar, TUpdateCar } from "../../interface/cars.interface";
+import { Car } from "../../entities/cars.entities";
+import { AppDataSource } from "../../data-source";
+import { carResponseSchema } from "../../schemas/car.schema";
+
+export const updateCarrService = async (
+  carData: TCarUpdate,
+  carId: number
+): Promise<TCarResponse> => {
+  const carRepository: Repository<Car> = AppDataSource.getRepository(Car);
+
+  const oldCar: Car | null = await carRepository.findOne({
+    where: {
+      id: carId,
+    },
+  });
+
+  const car: Car = carRepository.create({
+    ...oldCar,
+    ...carData,
+  });
+
+  await carRepository.save(car);
+  const updateCar = carResponseSchema.parse(car);
+
+  return updateCar;
+};
